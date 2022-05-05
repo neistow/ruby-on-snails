fb65a9e5062a546dfdbe80c106605941dc7097b3
neistow <glebchernigin236@gmail.com>
2022-05-05 21:53:13 +0300

    lab-12 done

--- main.rb
+++ main.rb
@@ -1,29 +1,21 @@
-class Integer
-  def to_a(base = 2)
-    return to_s(base).to_str.split("")
+def chunker(f_in)
+  f_size = File.size(f_in)
+  if f_size < 10
+    raise 'File is illegally smol.'
   end
-end
-
-
-new_books = {}
-old_books = {
-  'Design Patterns in Ruby' => ['Russ Olsen'],
-  'Eloquent Ruby' => ['Russ Olsen'],
-  'The Well-Grounded Rubyist' => ['David A. Black'],
-  'The Ruby Programming Language' => ['David Flanagan', 'Yukihiro Matsumoto'],
-  'Metaprogramming Ruby 2' => ['Paolo Perrotta'],
-  'Ruby Cookbook' => ['Lucas Carlson', 'Leonard Richardson'],
-  'Ruby Under a Microscope' => ['Pat Shaughnessy'],
-  'Ruby Performance Optimization' => ['Alexander Dymo'],
-  'The Ruby Way' => ['Hal Fulton', 'Andre Arko']
-}.each do |key|
-  key[1].each do |val|
-    if new_books.key?(val)
-      new_books[val] += 1
-    else
-      new_books[val] = 1
+  chunksize = f_size / 10
+  File.open(f_in, "r") do |fh_in|
+    until fh_in.eof?
+      File.open("x#{"%02d"%(fh_in.pos/chunksize)}.txt","w") do |fh_out|
+        fh_out << fh_in.read(chunksize)
+      end
     end
   end
 end
 
-puts new_books.sort_by { |x| x[1]}
+def count_files(dir, ext)
+  puts Dir[File.join(dir, '**', "*.#{ext}")].count { |file| File.file?(file) }
+end
+
+chunker("hefty_chonker.txt")
+count_files("./", "rb")
